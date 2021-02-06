311d9ada3580e18a22397a4b88005614f89569c7
0xHigos <1172860590@qq.com>
2021-02-06 23:37:34 +0800

    [add] add override files

--- java0705scala/src/test/java/Test.java
+++ java0705scala/src/test/java/Test.java
@@ -3,63 +3,43 @@ import java.util.Deque;
 
 public class Test {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(7);
-        root.left = new TreeNode(3);
-        root.right = new TreeNode(15);
-        root.right.left = new TreeNode(9);
-        root.right.right = new TreeNode(20);
-        BSTIterator iterator = new BSTIterator(root);
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        System.out.println(iterator.hasNext());
-        System.out.println(iterator.next());
+        Solution2 s = new Solution2();
+        int maxScore = s.maxScore(new int[]{1, 2, 3, 4}, 2);
+        System.out.println(maxScore);
     }
 }
-class BSTIterator {
-    private Deque<TreeNode> stack;
 
-    public BSTIterator(TreeNode root) {
-        stack = new ArrayDeque<>();
-        TreeNode cur = root;
-        while(cur != null){
-            if(cur.left == null){
-                stack.add(new TreeNode(cur.val));
-                cur = cur.right;
-            }else{
-                TreeNode pre = cur.left;
-                while(pre.right != null && pre.right != cur){
-                    pre = pre.right;
-                }
-                if(pre.right == null){
-                    pre.right = cur;
-                    cur = cur.left;
-                }else{
-                    pre.right = null;
-                    stack.add(new TreeNode(cur.val));
-                    cur = cur.right;
-                }
-            }
+class Solution2 {
+    public int maxScore(int[] cardPoints, int k) {
+        int sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += cardPoints[i];
         }
-    }
-
-    public int next() {
-        return stack.pop().val;
-    }
-
-    public boolean hasNext() {
-        return !stack.isEmpty();
+        int maxSum = sum;
+        for(int i = 0; i < k; i++)
+        {
+            sum += cardPoints[cardPoints.length - i - 1];
+            sum -= cardPoints[k - i - 1];
+            maxSum = Math.max(sum,maxSum);
+        }
+        return maxSum;
     }
 }
-
 class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
